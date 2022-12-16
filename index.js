3ef97d1892c9dfb14b1b0679540bf972312edeb0
Mukhlisa <mhlsmahmudova@gmail.com>
2022-12-16 14:34:29 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -42,18 +42,26 @@
  /* 2 */
 
  let name = prompt("What is your name?")
- let age = prompt("How old are you?")
+
  let cash = prompt("How much money do you have?")
 
 
- if(name[0] === 'a' || name[0] === 'A' && age >= 20 && age <= 40 && cash >= 100){
+ if(name[0] === 'a' || name[0] === 'A' && cash >= 100){
+    
+   let age = prompt("How old are you?")
+  if( age >= '20' && age <= '40'){
 
-    let member = prompt("How many people are you?")
-    if(member <= 10){
-        alert("perfect")
-    } else {
-        alert("oh sorry we do not have enough space")
-    }
+   let member = prompt("How many people are you?")
+   if(member <= 10){
+       alert("perfect")
+   } else {
+       alert("oh sorry we do not have enough space")
+   }
+  } else{
+   alert('bye')
+  }
+
+
  } else {
     alert('bye')
  }
