d6a4c23a1ef8fd59c8f545bd038379c29e411890
Mukhlisa <mhlsmahmudova@gmail.com>
2023-01-09 21:04:43 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,53 @@
 
-    /* 1 */ 
- /* let name = "Alex"
- let money  = 10000
- let account = 7777
+// Напишите функцию которая заполняет массив нужными данными 
+ let arr = ['a', 'a', 'a', 'a'] // -> [4, 'a'] 
+  let arr2 = [];
 
- let user = prompt('What is your name?')
-
-
- if(user === "Alex"){
-
-    let number = prompt('What is your account number')
-    if(number === "7777" ){ 
-
-        let cash = prompt("How much to cash out?") 
-        if(cash <= 10000){
-           alert("Perfect")
-
-           if (cash <= 10000 ) {
-            console.log(10000-cash)
-           } else {
-            alert ("Insufficient funds")
-           }
-
-        } else {
-            alert ("Insufficient funds")
-          }
-
-    } else{
-     alert("User is not found bye")
+  for (i in arr) {
+    if (arr2[arr[i]] != undefined) {
+      (arr2[arr[i]]++)
+    } else {
+      (arr2[arr[i]] = 1)
     }
-
-    
-
- } else {
-    alert("User is not found bye")
- } 
+  }
+  console.log(arr2);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  //2
+// compact
+// Убрать все не желательные элементы 
+/* let arr = ['hello', null, 0, true, false, 1, undefined, '', NaN]
+console.log (arr.filter(Boolean))
  */
 
 
- 
- /* 2 */
-
- let name = prompt("What is your name?")
- let age = prompt("How old are you?")
- let cash = prompt("How much money do you have?")
 
 
- if(name[0] === 'a' || name[0] === 'A' && age >= 20 && age <= 40 && cash >= 100){
 
-    let member = prompt("How many people are you?")
-    if(member <= 10){
-        alert("perfect")
-    } else {
-        alert("oh sorry we do not have enough space")
-    }
- } else {
-    alert('bye')
- }
+//3
+// unique
+// Оставить в массиве только оригинал
+/*  let arr = ['a', 'b', true, 'a', false, 'b', true]
+console.log(arr.filter((v, i, a) => a.indexOf(v) === i)) */
+
+
+
+
+
+// 4
+// Проверить массив на схожесть
+/* let arr = [1,2,3,4,5]
+let arr2 = [1,2,3,4,5]
+
+console.log(arr[0] === arr2[0]) */
